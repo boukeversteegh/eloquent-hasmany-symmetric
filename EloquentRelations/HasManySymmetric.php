36a993ae64a323603fa5b2cda183ceded1fd8c18
Bouke Versteegh <info@boukeversteegh.nl>
2015-11-18 20:31:05 +0100

    Restructure package

--- EloquentRelations/HasManySymmetric.php
+++ EloquentRelations/HasManySymmetric.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Boukeversteegh\EloquentHasManySymmetric;
+namespace EloquentRelations;
 
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\Relation;
